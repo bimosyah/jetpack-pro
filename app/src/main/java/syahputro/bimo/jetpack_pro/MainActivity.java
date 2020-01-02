d071f5161972b48879203c1ee27404a8f309cc9f
bimosyah <bimosyahputro88@gmail.com>
2020-01-02 07:58:43 +0700

    first commit

---
+++ app/src/main/java/syahputro/bimo/jetpack_pro/MainActivity.java
@@ -0,0 +1,14 @@
+package syahputro.bimo.jetpack_pro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
